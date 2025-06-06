8936b866c741353e0e4480a65430269f270feb94
DJ <du.jing2@northeastern.edu>
2025-06-06 14:06:39 -0400

    first

---
+++ web102/timetabled/src/App.jsx
@@ -0,0 +1,17 @@
+import Calendar from './components/Calendar'
+import './App.css'
+
+  const App = () => {
+
+    return (
+      <div className="App">
+        <><h1>Itineray for 7 Days in Chicago 🏙</h1></>
+        <><h2>Welcome to Chicago, Natalia! Check out this calendar to get to know the city and see all the sights during your stay.</h2></>
+        <Calendar />
+  
+        
+      </div>
+    )
+  }
+
+export default App
---
+++ web102/timetabled/src/components/Event.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Event = (props) => {
+    return (
+    <div className="Event">  
+        <><h5>🏙</h5></>
+        </div> 
+    )
+    
+}
+
+
+export default Event
